2b80476008888a4f20d1fcaa2533961007f1d0e3
Ivan5 <converse_ivan@hotmail.com>
2019-04-11 13:41:22 -0500

    Add styles to Person component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+import "./App.css";
+
+const App = () => <PersonList />;
+
+const PersonList = () => <Person />;
+
+const Person = props => {
+  const url = "https://randomuser.me/api/portraits/thumb/lego/1.jpg";
+  return (
+    <div className="person">
+      <img src={url} alt="" />
+      <div>
+        <h4>Name</h4>
+        <h4>Job</h4>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
